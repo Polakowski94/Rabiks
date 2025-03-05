76595e630ed6486d74451b7b4fd71c74c09db5e7
Polakowski94 <pola.grzegorz@gmail.com>
2025-03-05 17:54:15 +0100

    Added RemoteTimerClock to count time

---
+++ RemoteTimer/Clock/RemoteTimerClock.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteTimer.Clock
+{
+    public class RemoteTimerClock
+    {
+        public Action<int> OnStopwatchSecondPassed;
+      
+        public DateTime StartTime { get; private set; }
+        private Thread StopwatchThread { get; set; }
+
+        public RemoteTimerClock(Action<int> updateTimerMethod)
+        {
+            OnStopwatchSecondPassed += updateTimerMethod;
+        }
+
+        public void Start()
+        {
+            StartTime = DateTime.Now;
+            StopwatchThread = new Thread(new ThreadStart(WorkStopwatchThread));
+            StopwatchThread.Start();
+        }
+
+        public void Stop()
+        {
+            StopwatchThread.Interrupt();
+        }
+
+        private void WorkStopwatchThread()
+        {
+            var secondsPassed = 0;
+
+            while (true)
+            {
+                try
+                {
+                    Thread.Sleep(1000);
+                }
+                catch
+                {
+                    break;
+                }
+
+                secondsPassed++;
+                OnStopwatchSecondPassed.Invoke(secondsPassed);
+            }
+        }
+    }
+}
